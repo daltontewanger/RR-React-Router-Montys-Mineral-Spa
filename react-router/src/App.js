72d88ba80abf7f2fa6e6dbc315c7d0560b9190aa
Dalton Tewanger <daltontewanger@yahoo.com>
2024-04-12 13:43:15 -0700

    Updated styling to practice with react bootstrap

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import Home from "./components/Home";
 import About from "./components/About";
 import Packages from "./components/Packages";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/esm/Container";
 
 function App() {
   const packages = [
@@ -20,26 +22,35 @@ function App() {
         <header>
           <h1 className="title">Welcome to Monty's Mineral SPA</h1>
 
-          <div className="navBar">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-              <Link to="/about">About</Link>
-              </li>
-              <li>
-              <Link to="/packages"> Our Packages</Link>
-              </li>
-            </ul>
-          </div>
+          <Container>
+            <Nav defaultActiveKey="/" variant="tabs" fill>
+              <Nav.Item>
+                <Nav.Link href="/">
+                  <Link to="/">Home</Link>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey={"aboutPage"}>
+                  <Link to="/about">About Us</Link>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey={"packagesPage"}>
+                  <Link to="/packages">Our Packages</Link>
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Container>
         </header>
 
         <div className="display">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/packages" element={<Packages packages={packages}/>} />
+            <Route
+              path="/packages"
+              element={<Packages packages={packages} />}
+            />
           </Routes>
         </div>
       </Router>
